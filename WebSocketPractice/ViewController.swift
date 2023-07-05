2061cd52a572bcff86866d8a92ed365fa56d205e
roargenie <lazyhoper987@gmail.com>
2023-07-05 16:27:46 +0900

    test

--- WebSocketPractice/ViewController.swift
+++ WebSocketPractice/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var abc = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
