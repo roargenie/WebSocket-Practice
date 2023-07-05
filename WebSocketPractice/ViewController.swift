bd1254049b82a5722a7187bfeec4307b46488b8d
roargenie <lazyhoper987@gmail.com>
2023-07-05 16:21:57 +0900

    git init

---
+++ WebSocketPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebSocketPractice
+//
+//  Created by 이명진 on 2023/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
